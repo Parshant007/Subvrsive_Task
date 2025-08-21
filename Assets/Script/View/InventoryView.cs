f3b6dfec3e89cf1c9d3b591252d14e73acf25a20
LAPTOP-B5EI7JRQ\AloneSurvival <harsh.hbsb@gmail.com>
2025-08-21 08:36:56 +0530

    Inventory selector functionality implemen

--- Assets/Script/View/InventoryView.cs
+++ Assets/Script/View/InventoryView.cs
@@ -2,44 +2,71 @@ using NUnit.Framework;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System;
 namespace InventoryCanvasHandler
 {
     public class InventoryView : MonoBehaviour
     {
         [SerializeField]
         private List<GameObject> _inventoryComponenet = new List<GameObject>();
-        private const int R= 106;
-        private const int G = 79;
-        private const int B = 79;
-        private const int A = 136;
+
+        private static Action onSelectInventoryComponent;
+        private static Action onDe_SelectPrevious;
+
         public static int currentIndex { get; private set; }
-        Image img;
+        public static int previousIndex { get; private set; }
+        Image _inventory_Component_img;
         public void SelectAk47(int inventoryIndex)=> currentIndex = inventoryIndex;
         public void SelectPistol(int inventoryIndex)=> currentIndex = inventoryIndex;
-        public void SelectLaserGun(int inventoryIndex) => currentIndex = inventoryIndex;
+        public void SelectSniper(int inventoryIndex) => currentIndex = inventoryIndex;
         public void SelectKnife(int inventoryIndex) => currentIndex = inventoryIndex;
-        public void SelectTearGas(int inventoryIndex) => currentIndex = inventoryIndex;
-        public void SelectBomb(int inventoryIndex) => currentIndex = inventoryIndex;
+        public void SelectMachineGun(int inventoryIndex) => currentIndex = inventoryIndex;
+        public void SelectGrenad(int inventoryIndex) => currentIndex = inventoryIndex;
         public void SelectHealthBooster(int inventoryIndex) => currentIndex = inventoryIndex;
         public void SelectArmor(int inventoryIndex) => currentIndex = inventoryIndex;
-       
-        public void InventeryHoverOver()
+
+        private void Start()
         {
-            InventeryComponenetSelector(106, 79, 79, 36);
+            previousIndex = currentIndex = 0;
+            onSelectInventoryComponent?.Invoke();
         }
-        private void InventeryComponenetSelector(byte r, byte g, byte b, byte a)
+        private void OnEnable()
         {
-            img = _inventoryComponenet[currentIndex].GetComponent<Image>();
-            img.color = new Color32(r, g, b, a);
+            onSelectInventoryComponent += SelectInventoryComponent;
+            onDe_SelectPrevious += De_SelectPrevious;
+        }
+        private void OnDisable()
+        {
+            onSelectInventoryComponent -= SelectInventoryComponent;
+            onDe_SelectPrevious -= De_SelectPrevious;
+        }
+        public void InventoryHoverOver()
+        {
+            if (currentIndex.Equals(previousIndex)) return;
+            InventeryComponenetSelector(106, 79, 79, 36, currentIndex);
+        }
+        private void InventeryComponenetSelector(byte r, byte g, byte b, byte a, int selector)
+        {
+            _inventory_Component_img = _inventoryComponenet[selector].GetComponent<Image>();
+            _inventory_Component_img.color = new Color32(r, g, b, a);
         }
         public void SelectInventoryComponent()
         {
-            InventeryComponenetSelector(140, 29, 29, 136);
+            onDe_SelectPrevious?.Invoke();
+            InventeryComponenetSelector(140, 29, 29, 136, currentIndex);
         }
-        public void RestAllComponent()
+        public void InventoryHoverExit(int deselectIndex)
         {
-            InventeryComponenetSelector(63, 59, 59, 136);
+            if (currentIndex.Equals(previousIndex)) return;
+            InventeryComponenetSelector(63, 59, 59, 136, deselectIndex);
         }
 
+        #region
+        private void De_SelectPrevious()
+        {
+            InventoryHoverExit(previousIndex);
+            previousIndex = currentIndex;
+        }
+        #endregion
     }
 }
